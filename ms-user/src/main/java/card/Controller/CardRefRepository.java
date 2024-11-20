6d8eccc555018785238010b89ddbf8a5e7377b01
RemiLeCodeur <rem.louedec@laposte.net>
2024-11-20 08:37:16 +0100

    fix archi

--- ms-user/src/main/java/card/Controller/CardRefRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.card.Controller;
-
-import com.cpe.springboot.card.model.CardReference;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CardRefRepository extends CrudRepository<CardReference, Integer> {
-
-}
