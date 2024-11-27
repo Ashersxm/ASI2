7610cee63cb09c4bcc2609ef33a521e0d4aabe9f
Rémi Louedec <remi.louedec@orange.com>
2024-11-27 17:37:16 +0100

    ajout du chat dans la page home

--- front_react/src/pages/Home.jsx
+++ front_react/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Box, Container, Grid, Button } from '@mui/material';
 import Header from './Header';
+import Chat from './Chat';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SellIcon from '@mui/icons-material/Sell';
 import ScienceIcon from '@mui/icons-material/Science';
@@ -17,78 +18,98 @@ const Home = () => {
 
   console.log(users); // Logs the list of users once they are fetched
 
+  const user = useSelector(state => state.user);
+  console.log(user);
+
   return (
     <div>
       <Header />
-      {/* Main Content */}
-      <Box sx={{ py: 4 }}>
-        <Container maxWidth="md">
-          <Grid container spacing={4} justifyContent="center" alignItems="stretch">
-            {/* Sell Button */}
-            <Grid item xs={12} sm={6} md={4}>
-              <Button
-                variant="contained"
-                color="primary"
-                fullWidth
-                href="/card/SellPage"
-                startIcon={<SellIcon />}
-                size="large"
-                sx={{
-                  height: '100%',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  textTransform: 'none',
-                }}
-              >
-                Sell
-              </Button>
-            </Grid>
+      <Container maxWidth="lg" sx={{ mt: 4 }}>
+        <Grid container spacing={2}>
+          {/* Colonne de gauche pour le Chat */}
+          <Grid item xs={12} md={3}>
+            <Box
+              sx={{
+                border: '1px solid #e0e0e0',
+                borderRadius: 2,
+                p: 2,
+                height: '100%',
+                boxSizing: 'border-box',
+              }}
+            >
+              <Chat username={user?.user?.surName} />
+            </Box>
+          </Grid>
 
-            {/* Buy Button */}
-            <Grid item xs={12} sm={6} md={4}>
-              <Button
-                variant="contained"
-                color="secondary"
-                fullWidth
-                href="/card/BuyPage"
-                startIcon={<ShoppingCartIcon />}
-                size="large"
-                sx={{
-                  height: '100%',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  textTransform: 'none',
-                }}
-              >
-                Buy
-              </Button>
-            </Grid>
+          {/* Colonne de droite pour le contenu principal */}
+          <Grid item xs={12} md={9}>
+            <Grid container spacing={4}>
+              {/* Sell Button */}
+              <Grid item xs={12} sm={6} md={4}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  href="/card/SellPage"
+                  startIcon={<SellIcon />}
+                  size="large"
+                  sx={{
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textTransform: 'none',
+                  }}
+                >
+                  Sell
+                </Button>
+              </Grid>
 
-            {/* Create Button */}
-            <Grid item xs={12} sm={6} md={4}>
-              <Button
-                variant="contained"
-                color="success"
-                fullWidth
-                href="./forms/formCardCreation"
-                startIcon={<ScienceIcon />}
-                size="large"
-                sx={{
-                  height: '100%',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  textTransform: 'none',
-                }}
-              >
-                Create
-              </Button>
+              {/* Buy Button */}
+              <Grid item xs={12} sm={6} md={4}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  fullWidth
+                  href="/card/BuyPage"
+                  startIcon={<ShoppingCartIcon />}
+                  size="large"
+                  sx={{
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textTransform: 'none',
+                  }}
+                >
+                  Buy
+                </Button>
+              </Grid>
+
+              {/* Create Button */}
+              <Grid item xs={12} sm={6} md={4}>
+                <Button
+                  variant="contained"
+                  color="success"
+                  fullWidth
+                  href="./forms/formCardCreation"
+                  startIcon={<ScienceIcon />}
+                  size="large"
+                  sx={{
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textTransform: 'none',
+                  }}
+                >
+                  Create
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
-        </Container>
-      </Box>
+        </Grid>
+      </Container>
     </div>
   );
 };
